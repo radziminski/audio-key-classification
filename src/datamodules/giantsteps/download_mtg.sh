0b0c49cfc8a93ea08801519bded3e53c353eb279
kamieniarzk <kamieniarzk@gmail.com>
2022-12-13 09:32:50 +0100

    fix(GiantSteps MTG): fix download script (works on macOS)

--- src/datamodules/giantsteps/download_mtg.sh
+++ src/datamodules/giantsteps/download_mtg.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -
 
+# based on the script and annotations at https://github.com/GiantSteps/giantsteps-mtg-key-dataset
+
 LABEL_FILES=./labels/*
 BASEURL=http://www.cp.jku.at/datasets/giantsteps/mtg_key_backup/
 AUDIOPATH=./audio/
 
 handle_label() {
-  label_file=$1
+  label_file=${1}
   label=$(cat "$label_file")
   filename=$(basename "$label_file")
   mp3filename="${filename%.*}".mp3
   mp3url=${BASEURL}${mp3filename}
   audio_out_path=${AUDIOPATH}$label
-  audio_out_path=$(echo $audio_out_path | sed 's/\s\+/-/g')
-  audio_out_path=$(echo $audio_out_path | sed -E 's/^([^-]*-[^-]*)-.*$/\1/')
-
+  audio_out_path=$(echo $audio_out_path | sed 's/[[:space:]]/-/g')
+  audio_out_path=$(echo $audio_out_path | sed 's/^([^-]*-[^-]*)-.*/\$1/')
+  echo ${audio_out_path}
   mkdir -p ${audio_out_path}
 
   audio_out_path=$audio_out_path/$mp3filename
@@ -33,8 +35,11 @@ wget https://github.com/GiantSteps/giantsteps-mtg-key-dataset/archive/refs/heads
 unzip -j master.zip 'giantsteps-mtg-key-dataset-master/annotations/key/*' -d labels
 
 for label_file in $LABEL_FILES; do
-  handle_label label_file
+  handle_label ${label_file}
   ((count++))
 done
+
+rm ./master.zip
+rm ./labels
 
 printf "Downloaded: ${count} files\n"
