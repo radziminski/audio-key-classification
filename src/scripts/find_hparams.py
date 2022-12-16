1d108f28acff10687b0cef3e80c67c7e9f12f2f7
radziminski <radziminski.j@gmail.com>
2022-12-16 14:26:34 +0100

    feat(): added hparams search script

---
+++ src/scripts/find_hparams.py
@@ -0,0 +1,55 @@
+import logging
+
+import pyrootutils
+
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+from typing import List, Optional, Tuple
+
+import hydra
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
+from pytorch_lightning.loggers import LightningLoggerBase
+
+from src import utils
+
+
+@utils.task_wrapper
+def find_params(cfg: DictConfig) -> Tuple[dict, dict]:
+    datamodule = None
+    if cfg.datamodule_type == "image":
+        datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule.image)
+    if cfg.datamodule_type == "audio":
+        datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule.audio)
+
+    model: LightningModule = hydra.utils.instantiate(cfg.model)
+
+    trainer: Trainer = hydra.utils.instantiate(
+        cfg.trainer,
+    )
+    tuner = trainer.tuner
+    lr_finder = tuner.lr_find(model, datamodule)
+
+    lr = lr_finder.suggestion()
+    print(f"Best initial learning rate: {lr}")
+    lr_finder.plot()
+
+    tuner.scale_batch_size(model=model, datamodule=stanford_dogs_dm)
+
+
+print(f"Best batch size: {stanford_dogs_dm.batch_size}")
+
+
+@hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
+def main(cfg: DictConfig) -> Optional[float]:
+    find_params(cfg)
+
+
+if __name__ == "__main__":
+    main()
