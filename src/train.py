06ddf27478f7a8d2ae381b2708097f10837441de
kamieniarzk <kamieniarzk@gmail.com>
2022-12-16 16:17:57 +0100

    fix(train): fix imoprt errors caused by PyCharm's optimize imports functionality

--- src/train.py
+++ src/train.py
@@ -1,14 +1,6 @@
 import logging
-from typing import List, Optional, Tuple
 
-import hydra
 import pyrootutils
-import pytorch_lightning as pl
-from omegaconf import DictConfig
-from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
-from pytorch_lightning.loggers import LightningLoggerBase
-
-from src import utils
 
 root = pyrootutils.setup_root(
     search_from=__file__,
@@ -17,11 +9,35 @@ root = pyrootutils.setup_root(
     dotenv=True,
 )
 
+from typing import List, Optional, Tuple
+
+import hydra
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
+from pytorch_lightning.loggers import LightningLoggerBase
+
+from src import utils
+
 log = utils.get_pylogger(__name__)
 
 
 @utils.task_wrapper
 def train(cfg: DictConfig) -> Tuple[dict, dict]:
+    """Trains the model. Can additionally evaluate on a testset, using best weights obtained during
+    training.
+
+    This method is wrapped in optional @task_wrapper decorator which applies extra utilities
+    before and after the call.
+
+    Args:
+        cfg (DictConfig): Configuration composed by Hydra.
+
+    Returns:
+        Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
+    """
+
+    # set seed for random number generators in pytorch, numpy and python.random
     if cfg.get("seed"):
         pl.seed_everything(cfg.seed, workers=True)
     logging.getLogger("PIL").setLevel(logging.WARNING)
@@ -86,10 +102,16 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
 
 @hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
 def main(cfg: DictConfig) -> Optional[float]:
+
+    # train the model
     metric_dict, _ = train(cfg)
+
+    # safely retrieve metric value for hydra-based hyperparameter optimization
     metric_value = utils.get_metric_value(
         metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
     )
+
+    # return optimized metric
     return metric_value
 
 
