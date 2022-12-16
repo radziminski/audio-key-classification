0ba6ce46909f859667890e61211494d6c05bc77c
radziminski <radziminski.j@gmail.com>
2022-12-16 10:34:44 +0100

    feat(*) updated dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,4 @@
 
 pip install -r requirements.txt
 sudo apt-get install ffmpeg mediainfo sox libsox-fmt-mp3
-add-apt-repository -y ppa:savoury1/ffmpeg4
-apt-get -qq install -y ffmpeg
 export HYDRA_FULL_ERROR=1
