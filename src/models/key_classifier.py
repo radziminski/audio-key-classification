5c38d3292affe06d9bc4fdd8c87af076be96918a
radziminski <radziminski.j@gmail.com>
2022-12-19 22:48:12 +0100

    feat(): added test mirex loss

--- src/models/key_classifier.py
+++ src/models/key_classifier.py
@@ -3,33 +3,41 @@ from pytorch_lightning import LightningModule
 from torchmetrics import MaxMetric, MeanMetric
 from torchmetrics.classification.accuracy import Accuracy
 
-from src.utils import mirex
+from src.utils.mirex import (
+    mirex_score,
+    get_maps,
+    get_perfect_fifths,
+    get_relative,
+    get_parallel,
+)
 
-
-#  keys_idx_map, keys_idx_map_inv = get_maps()
-#         x, y = batch
-#         mirex_y = torch.zeros(len(y), 24).to('cuda')
-#         for index, y_true_t in enumerate(y):
-#             y_true = int(y_true_t)
-#             mirex_y[index][y_true] = 0.5
-
-#             true_key = keys_idx_map[y_true]
-#             first, second = get_perfect_fifths(true_key)
-#             first, second = keys_idx_map_inv[first], keys_idx_map_inv[second]
-
-#             relative = keys_idx_map_inv[get_relative(true_key)]
-#             parallel = keys_idx_map_inv[get_parallel(true_key)]
-
-#             mirex_y[index][first] = 0.175
-#             mirex_y[index][second] = 0.175
-#             mirex_y[index][relative] = 0.1
-#             mirex_y[index][parallel] = 0.05
-
-#         logits = self.forward(x)
-#         loss = self.criterion(logits, mirex_y)
-#         preds = torch.argmax(logits, dim=1)
-
-#         return loss, preds, y
+class_to_idx = {
+    "A-maj": 0,
+    "A-min": 1,
+    "Ab-maj": 2,
+    "Ab-min": 3,
+    "B-maj": 4,
+    "B-min": 5,
+    "Bb-maj": 6,
+    "Bb-min": 7,
+    "C-maj": 8,
+    "C-min": 9,
+    "D-maj": 10,
+    "D-min": 11,
+    "Db-maj": 12,
+    "Db-min": 13,
+    "E-maj": 14,
+    "E-min": 15,
+    "Eb-maj": 16,
+    "Eb-min": 17,
+    "F-maj": 18,
+    "F-min": 19,
+    "G-maj": 20,
+    "G-min": 21,
+    "Gb-maj": 22,
+    "Gb-min": 23,
+}
+idx_to_class = {v: k for k, v in class_to_idx.items()}
 
 
 class KeyClassifier(LightningModule):
@@ -59,17 +67,36 @@ class KeyClassifier(LightningModule):
         self.val_acc_best.reset()
 
     def step(self, batch):
+        keys_idx_map, keys_idx_map_inv = get_maps()
         x, y = batch
+        mirex_y = torch.zeros(len(y), 24).to("cuda")
+        for index, y_true_t in enumerate(y):
+            y_true = int(y_true_t)
+            mirex_y[index][y_true] = 0.5
+
+            true_key = keys_idx_map[y_true]
+            first, second = get_perfect_fifths(true_key)
+            first, second = keys_idx_map_inv[first], keys_idx_map_inv[second]
+
+            relative = keys_idx_map_inv[get_relative(true_key)]
+            parallel = keys_idx_map_inv[get_parallel(true_key)]
+
+            mirex_y[index][first] = 0.175
+            mirex_y[index][second] = 0.175
+            mirex_y[index][relative] = 0.1
+            mirex_y[index][parallel] = 0.05
+
         logits = self.forward(x)
-        loss = self.criterion(logits, y)
+        loss = self.criterion(logits, mirex_y)
         preds = torch.argmax(logits, dim=1)
+
         return loss, preds, y
 
     def _test_step(self, batch):
         x, y = batch
         logits = self.forward(x)
         preds = torch.argmax(logits, dim=1)
-        loss = mirex.mirex_score(preds, y)
+        loss = mirex_score(preds, y)
         return loss, preds, y
 
     def training_step(self, batch, batch_idx: int):
