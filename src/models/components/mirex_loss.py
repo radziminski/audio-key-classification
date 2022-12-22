19c2f6320d062aea1d96a5cda10a493d944fbec8
radziminski <radziminski.j@gmail.com>
2022-12-22 18:44:27 +0100

    fix(MIREX): fixed bug in mirex

--- src/models/components/mirex_loss.py
+++ src/models/components/mirex_loss.py
@@ -9,11 +9,15 @@ MIREX_LOSS_VERSION_MAP = {
 
 
 class MirexLoss(torch.nn.Module):
-    def __init__(self, version="v1", device="gpu", criterion=None):
+    def __init__(self, version="v1", device="gpu"):
         super(MirexLoss, self).__init__()
         self.loss = MIREX_LOSS_VERSION_MAP[version]
         self.device = "cuda" if device == "gpu" else device
-        self.criterion = criterion
+        self.criterion = (
+            torch.nn.CrossEntropyLoss()
+            if version == "v1"
+            else torch.nn.BCEWithLogitsLoss(reduction="none")
+        )
 
     def forward(self, prediction, true_class):
         return self.loss(prediction, true_class, self.criterion, device=self.device)
