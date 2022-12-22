ae843731fa6445a6c6bb191585b3c1f509650d1e
radziminski <radziminski.j@gmail.com>
2022-12-22 19:48:27 +0100

    fix(MirexMetric): fixed mirex metric

--- src/models/components/mirex_metric.py
+++ src/models/components/mirex_metric.py
@@ -6,14 +6,13 @@ from src.utils import mirex
 class MirexMetric(Metric):
     def __init__(self):
         super().__init__()
-        self.add_state("mirex", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("mirex", default=torch.tensor(0.0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
 
     def update(self, preds: torch.Tensor, target: torch.Tensor):
-        preds, target = self._input_format(preds, target)
         assert preds.shape == target.shape
 
-        self.mirex += mirex.mirex_score(target, preds)
+        self.mirex += mirex.mirex_score(target, preds).sum()
         self.total += target.numel()
 
     def compute(self):
