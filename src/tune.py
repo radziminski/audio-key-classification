053e942b71df68dde288f11380cef3feb3749ef5
kamieniarzk <kamieniarzk@gmail.com>
2023-01-05 18:50:03 +0100

    feat(tune): add tune task

---
+++ src/tune.py
@@ -0,0 +1,111 @@
+import logging
+import gc
+import pyrootutils
+import torch
+
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+from typing import List, Optional, Tuple
+
+import hydra
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
+from pytorch_lightning.loggers import LightningLoggerBase
+
+from src import utils
+
+log = utils.get_pylogger(__name__)
+
+
+@utils.task_wrapper
+def train(cfg: DictConfig) -> Tuple[dict, dict]:
+    if cfg.get("seed"):
+        pl.seed_everything(cfg.seed, workers=True)
+    logging.getLogger("PIL").setLevel(logging.WARNING)
+
+    datamodule = None
+    if cfg.datamodule_type == "image":
+        log.info(f"Instantiating datamodule <{cfg.datamodule.image._target_}>")
+        datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule.image)
+    if cfg.datamodule_type == "audio":
+        log.info(f"Instantiating datamodule <{cfg.datamodule.audio._target_}>")
+        datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule.audio)
+    if cfg.datamodule_type == "tensor":
+        log.info(f"Instantiating datamodule <{cfg.datamodule.torch._target_}>")
+        datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule.torch)
+
+    log.info(f"Instantiating model <{cfg.model._target_}>")
+    model: LightningModule = hydra.utils.instantiate(cfg.model)
+
+    log.info("Instantiating callbacks...")
+    callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
+
+    log.info("Instantiating loggers...")
+    logger: List[LightningLoggerBase] = utils.instantiate_loggers(cfg.get("logger"))
+
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(
+        cfg.trainer, callbacks=callbacks, logger=logger
+    )
+
+    object_dict = {
+        "cfg": cfg,
+        "datamodule": datamodule,
+        "model": model,
+        "callbacks": callbacks,
+        "logger": logger,
+        "trainer": trainer,
+    }
+
+    if logger:
+        log.info("Logging hyperparameters!")
+        utils.log_hyperparameters(object_dict)
+
+    if cfg.get("train"):
+        log.info("Starting training!")
+        trainer.tune(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+
+    train_metrics = trainer.callback_metrics
+
+    if cfg.get("test"):
+        log.info("Starting testing!")
+        ckpt_path = trainer.checkpoint_callback.best_model_path
+        if ckpt_path == "":
+            log.warning("Best ckpt not found! Using current weights for testing...")
+            ckpt_path = None
+        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+        log.info(f"Best ckpt path: {ckpt_path}")
+
+    test_metrics = trainer.callback_metrics
+
+    # merge train and test metrics
+    metric_dict = {**train_metrics, **test_metrics}
+
+    return metric_dict, object_dict
+
+
+@hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
+def main(cfg: DictConfig) -> Optional[float]:
+
+    # train the model
+    metric_dict, _ = train(cfg)
+
+    # safely retrieve metric value for hydra-based hyperparameter optimization
+    metric_value = utils.get_metric_value(
+        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
+    )
+
+    torch.cuda.empty_cache()  # to prevent RuntimeError: CUDA out of memory in between experiments
+    gc.collect()
+    # return optimized metric
+    return metric_value
+
+
+if __name__ == "__main__":
+    main()
